876f769b2aa778a58c668c626f9fb0492e3b8306
Lauren Kelly <laurenkelly@pantheon.io>
2018-02-03 14:13:45 -0500

    5.20

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+print('Hello' + ' World!')
+print('1+2=')
+print(1+2)
+print(8*4)
+print()
+print('the end')
+
+
+greeting = "Hello"
+# I guess I'll talk to myself for now, and take input later
+name = "Lauren"
+print(greeting + ', ' + name)
